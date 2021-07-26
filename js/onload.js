6dc1248f1590a7af60cef27ba2608d5db5941435
Jin SungWan <soma0sd@gmail.com>
2021-07-26 13:47:35 +0900

    sidebar mobile bug fix

--- js/onload.js
+++ js/onload.js
@@ -2,10 +2,15 @@
 function openSidebar() {
     var articleHeight = window.getComputedStyle(document.querySelector("main")).height;
     document.getElementById('close-sidebar').style.display = 'block';
-    document.getElementById('toc-sidebar').style.width = "250px";
+    document.getElementById('toc-sidebar').classList.add("show");
     document.getElementById('toc-sidebar').style.height = articleHeight;
 }
 function closeSidebar() {
     document.getElementById('close-sidebar').style.display = 'none';
-    document.getElementById('toc-sidebar').style.width = "0";
+    document.getElementById('toc-sidebar').classList.remove("show");
 }
+window.addEventListener('resize', function () {
+    var articleHeight = window.getComputedStyle(document.querySelector("main")).height;
+    document.getElementById('toc-sidebar').style.height = articleHeight;
+    closeSidebar();
+});
--- ts/onload.ts
+++ ts/onload.ts
@@ -2,11 +2,17 @@
 function openSidebar() {
     const articleHeight = window.getComputedStyle(document.querySelector("main")!).height
     document.getElementById('close-sidebar')!.style.display = 'block'
-    document.getElementById('toc-sidebar')!.style.width = "250px"
+    document.getElementById('toc-sidebar')!.classList.add("show")
     document.getElementById('toc-sidebar')!.style.height = articleHeight
 }
 
 function closeSidebar() {
     document.getElementById('close-sidebar')!.style.display = 'none'
-    document.getElementById('toc-sidebar')!.style.width = "0"
+    document.getElementById('toc-sidebar')!.classList.remove("show")
 }
+
+window.addEventListener('resize', () => {
+    const articleHeight = window.getComputedStyle(document.querySelector("main")!).height
+    document.getElementById('toc-sidebar')!.style.height = articleHeight
+    closeSidebar()
+})
